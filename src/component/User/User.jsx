4027c473901200cfa040e0bbf6e1ef856c3b6d61
marko-pantaleon <marko.evangelista.pantaleon@gmail.com>
2024-08-31 00:21:08 +0800

    Initial commit

---
+++ src/component/User/User.jsx
@@ -0,0 +1,11 @@
+function User() {
+    return (
+        <div>
+           <h1> Marko E. Pantaleon</h1>
+           <h2>BSIT-3B</h2>
+           <h3>I am an IT student.</h3>
+        </div>
+    );
+}
+
+export default User;
